de3687bb34f738d4ba339a3a34840ace5aa58df1
a.nikkholgh <alimemar80@gmail.com>
2022-02-02 15:31:15 +0330

    add to github

---
+++ js/Script-nav.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('.nav-fostrap li').click(function () {
+        $('ul.dropdown').slideUp("fast");
+        if ($(this).find('ul.dropdown').css('display') == 'none'){
+
+            $(this).find('ul.dropdown').slideDown("fast");
+        } else {
+            $(this).find('ul.dropdown').slideUp("fast");
+        }
+        
+        
+    });
+
+
+    $('.navbar-fostrap').click(function () {
+        $('.nav-fostrap').toggleClass('visible');
+        $('body').toggleClass('cover-bg');
+    });
+});
